d9f4ea92ac6ae12f68ac4b31f876912870c0be67
Wesley-John <wesleyjohn691@gmail.com>
2026-01-22 14:20:03 +0300

    Add function to perform integration using trapezoidal rule.

--- calculus.py
+++ calculus.py
@@ -2,3 +2,13 @@ import math
 
 def derivative(f, x, h=1e-5):
     return (f(x + h) - f(x - h)) / (2 * h)
+
+def integrate(f, a, b, n):
+    h = (b - a) / float(n)
+
+    integral_sum = 0.5 * h * (f(a) + f(b))
+
+    for i in range(1, int(n)):
+        integral_sum += h * f(a + i * h)
+
+    return integral_sum
